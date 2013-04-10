969818569d7118f9e4e943b794ea29ffd309b8df
Philips Emonina <emoninaphilips@gmail.com>
2013-04-10 13:12:04 +0100

    Branch1

---
+++ continueassignment2/src/ie/gcdassignment2/Sample1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ie.gcdassignment2;
+
+/**
+ * @author philips emonina
+ *
+ */
+public class Sample1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
